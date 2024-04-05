889b60c4787f28e2c2bbe11b2513fcdd6f66d439
as2947 <abot1998@gmail.com>
2024-04-05 16:25:13 -0700

    Got rid of methods to test routes

--- routes/ping.py
+++ routes/ping.py
@@ -3,9 +3,9 @@ from flask import jsonify
 from utils.auth import require_auth
 
 def init_app(app_instance):
-    @app_instance.app.route('/', methods=['POST'])
-    @app_instance.app.route('/home', methods=['POST'])
-    @app_instance.app.route('/ping', methods=['POST'])
+    @app_instance.app.route('/')
+    @app_instance.app.route('/home')
+    @app_instance.app.route('/ping')
     @require_auth
     def home():
         return jsonify({"message": f"You are connected to ItemInSight Heroku in {app_instance.flask_env} mode!"})
