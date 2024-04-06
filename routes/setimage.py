9ae452ecf99264244dea7ecb1619005f3af5eed1
as2947 <abot1998@gmail.com>
2024-04-06 04:25:46 -0700

    got rid of image name req

--- routes/setimage.py
+++ routes/setimage.py
@@ -4,8 +4,8 @@ import requests
 import os
 
 def init_app(app_instance):
-    @app_instance.app.route('/setimage/<path:image_path>', methods=['POST'])
-    def setimage(image_path):
+    @app_instance.app.route('/setimage/<path:image_dir>', methods=['POST'])
+    def setimage(image_dir):
         try:
 
             username = request.headers.get('username')
@@ -28,25 +28,23 @@ def init_app(app_instance):
             # Ensure the file is a PNG image
             if not file or not file.filename.lower().endswith('.png'):
                 return jsonify({"error": "Only PNG images are allowed."}), 400
-            
-            filename = image_path
 
             # Base URL of the first backend
             idb_base_url = app_instance.idb_base_url
             # Endpoint for saving the image
-            save_endpoint = f"{idb_base_url}/savefile/{username + "/" + filename}"
+            save_endpoint = f"{idb_base_url}/save/{username + "/" + image_dir}"
             # Authorization key
             auth_key = app_instance.idb_auth  # This should ideally be stored/configured securely
             
             # Prepare the file in a form that can be sent via POST request
-            files = {'file': (filename, file.stream, 'image/png')}
+            files = {'file': ("image.png", file.stream, 'image/png')}
             
             # Make a request to the first backend to save the image
             response = requests.post(save_endpoint, headers={"Authorization": auth_key}, files=files)
             
             # Handle the response from the first backend
             if response.status_code == 201:
-                return jsonify({"message": f"Image {filename} uploaded successfully."}), 201
+                return jsonify({"message": f"Image image.png uploaded successfully."}), 201
             else:
                 # Forward the error from the first backend
                 return jsonify({"error": "Failed to upload image"}), response.status_code
