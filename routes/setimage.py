03a00ffb9435a26e501b9cf682b13e60328eb2b5
as2947 <abot1998@gmail.com>
2024-04-06 02:37:48 -0700

    setimage idfk

--- routes/setimage.py
+++ routes/setimage.py
@@ -1,10 +1,9 @@
 # routes/setimage.py
-from flask import request, jsonify
+from flask import jsonify, request, Response
 import requests
-import os
 
 def init_app(app_instance):
-    @app_instance.app.route('/setimage/<path:image_name>', methods=['POST'])
+    @app_instance.app.route('/setimage/<path:image_name>', methods=['GET'])
     def setimage(image_name):
         try:
             username = request.headers.get('username')
@@ -13,34 +12,23 @@ def init_app(app_instance):
             user = app_instance.get_authed_user(app_instance, username, password)
             if not user:
                 return jsonify({"error": "Unauthorized: Incorrect username or password"}), 401
-            
-            # Check if there is a file in the request
-            if 'image' not in request.files:
-                return jsonify({"error": "No image file in the request."}), 400
-            
-            file = request.files['image']
-
-            filename = image_name
 
             # Base URL of the first backend
             idb_base_url = app_instance.idb_base_url
-            # Endpoint for saving the image
-            save_endpoint = f"{idb_base_url}/save/{filename}"
+            # Endpoint for the specific image
+            image_endpoint = f"{idb_base_url}/save/{username + "/" + image_name}"
             # Authorization key
             auth_key = app_instance.idb_auth  # This should ideally be stored/configured securely
             
-            # Prepare the file in a form that can be sent via POST request
-            files = {'file': (filename, file.stream, 'image/png')}
+            # Make a request to the first backend to get the image
+            response = requests.get(image_endpoint, headers={"Authorization": auth_key})
             
-            # Make a request to the first backend to save the image
-            response = requests.post(save_endpoint, headers={"Authorization": auth_key}, files=files)
-            
-            # Handle the response from the first backend
-            if response.status_code == 201:
-                return jsonify({"message": f"Image {filename} uploaded successfully."}), 201
+            # If the request was successful, forward the image data
+            if response.status_code == 200:
+                return jsonify({"message": "Image saved successfully"}), 200
             else:
                 # Forward the error from the first backend
-                return jsonify({"error": "Failed to upload image"}), response.status_code
+                return jsonify({"error": "Failed to retrieve image"}), response.status_code
 
         except Exception as e:
             return jsonify({"error": str(e)}), 500
