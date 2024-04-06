ad5d72cdaa6998fa13e6c1f48ec2497af58d68c4
as2947 <abot1998@gmail.com>
2024-04-06 04:29:37 -0700

    save to savefile

--- routes/setimage.py
+++ routes/setimage.py
@@ -32,7 +32,7 @@ def init_app(app_instance):
             # Base URL of the first backend
             idb_base_url = app_instance.idb_base_url
             # Endpoint for saving the image
-            save_endpoint = f"{idb_base_url}/save/{username + "/" + image_dir}"
+            save_endpoint = f"{idb_base_url}/savefile/{username + "/" + image_dir}"
             # Authorization key
             auth_key = app_instance.idb_auth  # This should ideally be stored/configured securely
             
